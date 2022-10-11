28995d70bcfc31c8275b576d4bb8e1cc70f20c60
Jacobo Casado <casadojacobo@gmail.com>
2022-10-11 18:41:19 +0200

    Nearly finished.

--- 3/Test_RSA.java
+++ 3/Test_RSA.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -78,7 +79,9 @@ public class Test_RSA {
 				sourceFile = args[1];
 				destinationFile = args[2];
 			} catch (ArrayIndexOutOfBoundsException ex) {
-				System.out.println("In 'e' and 'd' modes you need to provide both surce and destination files");
+				System.out.println("In 'e' and 'd' modes you need to provide both source and destination files.");
+				System.out.println("Usage: java -jar SecureSec.jar d|e -input -output");
+
 				System.exit(-1);
 			}
 
@@ -92,7 +95,7 @@ public class Test_RSA {
 				path = Paths.get("./public.key");
 				bytes = Files.readAllBytes(path);
 			} catch (NullPointerException | IOException ex) {
-				System.out.println("It is not possible to acces the ./public.key file");
+				System.out.println("It is not possible to acces the ./public.key file. \n Try again.");
 				System.exit(-1);
 			}
 
@@ -102,7 +105,7 @@ public class Test_RSA {
 				KeyFactory keyfactory = KeyFactory.getInstance("RSA");
 				publicKey = keyfactory.generatePublic(keyspec);
 			} catch (NullPointerException | InvalidKeySpecException ex) {
-				System.out.println("Worg public key format");
+				System.out.println("Wrong public key format");
 				System.exit(-1);
 			}
 
@@ -112,7 +115,8 @@ public class Test_RSA {
 				path = Paths.get("./private.key");
 				private_key = Files.readAllBytes(path);
 			} catch (NullPointerException | IOException ex) {
-				System.out.println("It is not possible to acces the ./private.key file");
+				System.out.println("./private.key file not found. \n Try again generating keys.");
+				System.out.println("Run: java -jar SecureSec.jar and ensure keys are created.");
 				System.exit(-1);
 			}
 
@@ -122,11 +126,11 @@ public class Test_RSA {
 				PKCS8EncodedKeySpec keyspec2 = new PKCS8EncodedKeySpec(decrypted_private_key);
 				KeyFactory keyfactory2 = KeyFactory.getInstance("RSA");
 				privateKey = keyfactory2.generatePrivate(keyspec2);
-			} catch (NullPointerException | InvalidKeySpecException ex) {
-				System.out.println("Error creating private key. You might have entered bad the passphrase. \n 16 characters is not enough!");
+			} catch (InvalidKeySpecException ex) {
+				System.out.println("Error while decrypting private key. The passphrase is not right OR key might be corrupted.");
 				System.exit(-1);
 			} catch (Exception ex) {
-				System.out.println("Error while decripting the file" );
+				System.out.println("Error while decripting the file");
 				System.exit(-1);
 			}
 		}
@@ -140,10 +144,17 @@ public class Test_RSA {
 			// We randomly generate the session key.
 			sessionKey = rdmPassword();
 
+			byte[] plaintext_bytes = new byte[0];
 			// We read the text that we want to cypher.
 			File plaintext = new File(sourceFile);
 			// We convert the string text into bytes.
-			byte[] plaintext_bytes = method(plaintext);
+			try {
+				plaintext_bytes = method(plaintext);
+			} catch (IOException e) {
+				System.out.println("File specified to encrypt (" + sourceFile + ") not found.");
+				System.exit(-1);
+			}
+			
 			// We encrypt the text with our session key, randomly generated.
 			byte[] ciphertext_bytes = sCipher.encryptCBC(plaintext_bytes, sessionKey);
 			// Once the session key is used, we encrypt it with our public key.
@@ -161,8 +172,13 @@ public class Test_RSA {
 
 			byte[] message = SymmetricCipher.joinByteArray(cyphertext_plus_encryptedskey, signed);
 
-			try (FileOutputStream fos = new FileOutputStream(destinationFile)) {
-				fos.write(message);
+			try {
+				try (FileOutputStream fos = new FileOutputStream(destinationFile)) {
+					fos.write(message);
+				}
+			} catch(FileNotFoundException f){
+				System.out.println("Problem at destination file. Exiting.");
+				System.exit(-1);
 			}
 
 			System.out.println("Final file created as " + destinationFile + ". Exiting.");
@@ -173,7 +189,14 @@ public class Test_RSA {
 			// We read the text that we want to descypher.
 			File cyphertext_plus_things = new File(sourceFile);
 			// We convert the string text with all the things into bytes.
-			byte[] byte_cyphertext_plus_things = method(cyphertext_plus_things);
+			byte[] byte_cyphertext_plus_things = new byte[0];
+
+			try {
+				byte_cyphertext_plus_things = method(cyphertext_plus_things);
+			} catch (IOException e) {
+				System.out.println("File specified to decrypt (" + sourceFile + ") not found.");
+				System.exit(0);
+			}
 
 			// We split the message into the necessay varibles
 			byte[] sign = Arrays.copyOfRange(byte_cyphertext_plus_things, byte_cyphertext_plus_things.length - 128,
@@ -189,7 +212,7 @@ public class Test_RSA {
 			Boolean good_sign = r.verify(ciphertext_plus_key, sign, publicKey);
 
 			if (!good_sign) {
-				System.out.println("bad sign");
+				System.out.println("Bad signature. Private and public key does not match.");
 				System.exit(-1);
 			}
 
@@ -199,8 +222,13 @@ public class Test_RSA {
 			// We decipher the session key
 			byte[] decripted_plainText = sCipher.decryptCBC(ciphertext, decripted_sessionKey);
 
-			try (FileOutputStream fos = new FileOutputStream(destinationFile)) {
-				fos.write(decripted_plainText);
+			try {
+				try (FileOutputStream fos = new FileOutputStream(destinationFile)) {
+					fos.write(decripted_plainText);
+				}
+			} catch(FileNotFoundException f){
+				System.out.println("Problem at destination file. Exiting.");
+				System.exit(-1);
 			}
 
 			System.out.println("Decrypted file created as " + destinationFile + ". Exiting.");
@@ -208,7 +236,7 @@ public class Test_RSA {
 
 		} else {
 			System.out.println("The arguments provided are invalid.");
-			System.out.println("java < g | e |d > [sourceFile] [destinationFile]");
+			System.out.println("Usage: java <g|e|d> [sourceFile] [destinationFile]");
 			System.exit(-1);
 		}
 	}
